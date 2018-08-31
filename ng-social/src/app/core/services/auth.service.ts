204cf7ef2d2f91e21d2c6a71f91dd9b073926b74
saibot777 <stefantrajkovic777@gmail.com>
2018-09-01 01:51:09 +0200

    authService update

--- ng-social/src/app/core/services/auth.service.ts
+++ ng-social/src/app/core/services/auth.service.ts
@@ -3,35 +3,23 @@ import { HttpClient, HttpHeaders, HttpResponse, HttpResponseBase } from '@angula
 import { Observable } from "rxjs";
 import { environment } from "../../../environments/environment";
 
+export const options = {   
+    headers: new HttpHeaders({'Content-Type': 'application/json'}),
+    withCredentials: true,
+};
+
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     constructor(private _http: HttpClient) {}
 
-    // const httpGetOptions =
-    //     {   
-    //         withCredentials: true,
-    //     }; 
-
     createUser(payload: any): Observable<any> {
-
-        const options =
-         {   
-           headers:
-            new HttpHeaders (
-                {   
-                    'Content-Type': 'application/json'
-                }
-            ),
-             withCredentials: true,
-         };
-
         return this._http.post(`${environment.API_BASE_URL}users/register`, payload, options)
     }
 
     loginUser(payload: any): Observable<any> {
-        return this._http.post(`${environment.API_BASE_URL}users/login`, payload)
+        return this._http.post(`${environment.API_BASE_URL}users/login`, payload, options)
     }
 
 }
