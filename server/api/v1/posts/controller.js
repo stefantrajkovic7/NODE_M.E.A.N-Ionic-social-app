1adf64ffddf14508372fc0b0fa09c0b1ad3e87e6
saibot777 <stefantrajkovic777@gmail.com>
2018-09-16 14:01:48 +0200

    GET Posts controller impl

--- server/api/v1/posts/controller.js
+++ server/api/v1/posts/controller.js
@@ -46,3 +46,15 @@ exports.create = (req, res) => {
                 .json({ message: 'Error occured' });
         })
 }
+
+exports.list = async (req, res) => {
+    try {
+        const posts = await Post.find({})
+            .populate('user')
+            .sort({ created: -1 });
+
+            return res.status(HttpStatus.OK).json({ message: 'Success', posts });
+    } catch (err) {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error Ocurred', err });
+    }
+}
--- server/api/v1/posts/index.js
+++ server/api/v1/posts/index.js
@@ -5,7 +5,7 @@ const router = express.Router({ mergeParams: true });
 const controller = require('./controller');
 
 router.post('/create', middleware.authenticate, controller.create);
-// router.get('/find', controller.find);
+router.get('/list', middleware.authenticate, controller.list);
 // router.get('/current', middleware.authenticate, controller.current);
 
 module.exports = router;
