afcf3bd63c33f13b907e392a168d511d87294d7f
saibot777 <stefantrajkovic777@gmail.com>
2018-09-30 13:36:32 +0200

    following UI ngrx initiated

--- ng-social/src/app/users/store/users.actions.ts
+++ ng-social/src/app/users/store/users.actions.ts
@@ -3,7 +3,10 @@ import { Action } from '@ngrx/store';
 export enum UsersActionTypes {
     LoadUsers = '[Users] Load Users',
     LoadUsersSuccess = '[Users] Load Users Successfully',
-    LoadUsersFail = '[Users] Load Users Failed'
+    LoadUsersFail = '[Users] Load Users Failed',
+    FollowingAction = '[Users] Following Action',
+    FollowingActionSuccess = '[Users] Following Action Successfull',
+    FollowingActionFail = '[Users] Following Action Failed',
 }
 
 /**
@@ -11,7 +14,6 @@ export enum UsersActionTypes {
  */
 export class LoadUsers implements Action {
     readonly type = UsersActionTypes.LoadUsers;
-    // constructor(public payload: any) { }
 }
 
 export class LoadUsersSuccess implements Action {
@@ -24,7 +26,25 @@ export class LoadUsersFail implements Action {
     constructor(public payload: any) { }
 }
 
+export class FollowingAction implements Action {
+    readonly type = UsersActionTypes.FollowingAction;
+    constructor(public payload: any) { }
+}
+
+export class FollowingActionSuccess implements Action {
+    readonly type = UsersActionTypes.FollowingActionSuccess;
+    constructor(public payload: any) { }
+}
+
+export class FollowingActionFail implements Action {
+    readonly type = UsersActionTypes.FollowingActionFail;
+    constructor(public payload: any) { }
+}
+
 export type UsersListActionsUnion =
     | LoadUsers
     | LoadUsersSuccess
     | LoadUsersFail
+    | FollowingAction
+    | FollowingActionSuccess
+    | FollowingActionFail
