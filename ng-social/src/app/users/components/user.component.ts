2bc7b8ab8e37168f09c241344e549cd50a18c5d2
saibot777 <stefantrajkovic777@gmail.com>
2018-09-30 11:34:15 +0200

    clean up

--- ng-social/src/app/users/components/user.component.ts
+++ ng-social/src/app/users/components/user.component.ts
@@ -11,10 +11,7 @@ export class UserComponent implements OnInit {
   @Input() users: any;
   loggedInUser: any;
 
-  constructor(private auth: AuthCookieService) {
-    // console.log(this.users[0].username + 'USERS')
-    
-  }
+  constructor(private auth: AuthCookieService) {}
 
   get usersList() {
     this.loggedInUser = this.auth.getUser()
@@ -22,12 +19,6 @@ export class UserComponent implements OnInit {
     return this.users;
   }
 
-  ngOnInit() {
-    this.loggedInUser = this.auth.getUser()
-    _.remove(this.users ? this.users : null, { username: this.loggedInUser.data.username })
-    console.log(this.users ? this.users : null)
-  }
-
-  // get usersList = 
+  ngOnInit() {}
 
 }
