ae949325853db1a31ee14010218fa32d7e9f5830
saibot777 <stefantrajkovic777@gmail.com>
2018-11-16 02:21:39 +0100

    notif dropdown ui in toolbar

--- ng-social/src/app/shared/components/toolbar.component.ts
+++ ng-social/src/app/shared/components/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Logout } from '../../core/store/actions';
 import { Router } from '@angular/router';
+import * as MUI from 'materialize-css';
 
 @Component({
   selector: 'app-toolbar',
@@ -14,6 +15,13 @@ export class ToolbarComponent implements OnInit {
   constructor(private store: Store<any>, private router: Router) {}
 
   ngOnInit() {
+
+    const dropDownElement = document.querySelector('.dropdown-trigger');
+    MUI.Dropdown.init(dropDownElement, {
+      aligment: 'right',
+      hover: true,
+      coverTrigger: false
+    })
     
   }
 
